2ab441bee9d8038ef6f2a8f3e7e8774c9396444b
JackLovel <gogkatsu@outlook.com>
2022-01-24 12:32:03 +0800

    添加员工的API

--- src/main/java/com/example/blogApi/controller/PersonController.java
+++ src/main/java/com/example/blogApi/controller/PersonController.java
@@ -1,8 +1,10 @@
 package com.example.blogApi.controller;
 
 import com.example.blogApi.entity.Person;
+import com.example.blogApi.exception.ResourceNotFoundException;
 import com.example.blogApi.repo.PersonRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,5 +36,18 @@ public class PersonController {
     @PostMapping
     public Person createPerson(@RequestBody Person person) {
         return personRepo.save(person);
+    }
+
+    /**
+     * 查找单个员工
+     * @param id 工号
+     * @return
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable long id) {
+        Person person = personRepo.findById(id).orElseThrow(()
+                ->new ResourceNotFoundException("用户不存在"));
+
+        return ResponseEntity.ok(person);
     }
 }
