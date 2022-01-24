fc03853a2d014c3f38812f7c73a2539363c59046
JackLovel <gogkatsu@outlook.com>
2022-01-24 19:58:32 +0800

    update

--- src/main/java/com/example/blogApi/controller/PersonController.java
+++ src/main/java/com/example/blogApi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.example.blogApi.entity.Person;
 import com.example.blogApi.exception.ResourceNotFoundException;
 import com.example.blogApi.repo.PersonRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -49,5 +50,38 @@ public class PersonController {
                 ->new ResourceNotFoundException("用户不存在"));
 
         return ResponseEntity.ok(person);
+    }
+
+    /**
+     * 查找单个员工
+     * @param id 工号
+     * @return
+     */
+    @PutMapping("{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable long id, @RequestBody Person personDetails) {
+        Person updatePerson = personRepo.findById(id).orElseThrow(()
+                ->new ResourceNotFoundException("用户不存在" + id));
+
+        updatePerson.setFirstName(personDetails.getFirstName());
+        updatePerson.setLastName(personDetails.getLastName());
+
+        personRepo.save(updatePerson);
+
+        return ResponseEntity.ok(updatePerson);
+    }
+
+    /**
+     * 删除单个员工
+     * @param id 工号
+     * @return
+     */
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deletePerson(@PathVariable long id) {
+        Person person = personRepo.findById(id).orElseThrow(()
+                ->new ResourceNotFoundException("用户不存在" + id));
+
+            personRepo.delete(person);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
